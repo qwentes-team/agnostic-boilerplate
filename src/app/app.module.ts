13726aabdc5011373ed9b9ed8eb28c6a533d0a3a
Matteo Barone <barmat90@gmail.com>
2019-01-09 15:41:37 +0100

    chore(app): test husky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { Feature1Component } from './features/feature1/feature1.component';
-import { Feature2Component } from './features/feature2/feature2.component';
-import { Feature3Component } from './features/feature3/feature3.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {Feature1Component} from './features/feature1/feature1.component';
+import {Feature2Component} from './features/feature2/feature2.component';
+import {Feature3Component} from './features/feature3/feature3.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Feature1Component,
-    Feature2Component,
-    Feature3Component,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-  ],
+  declarations: [AppComponent, Feature1Component, Feature2Component, Feature3Component],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}), EffectsModule.forRoot([])],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
